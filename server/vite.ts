f12f0f14dd3bc6a5e66f4a55b92ef5d7134bb4d8
retas-3535 <ali.eryasar@retasaltyapi.com>
2025-07-27 10:48:56 +0300

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -1,4 +1,4 @@
-import type { Express } from "express";
+import express, { type Express } from "express";
 import { createServer as createViteServer } from "vite";
 import { fileURLToPath } from "url";
 import path from "path";
